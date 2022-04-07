d5615180d23db9bd44d06ea2ea4545d69db930f9
krash76 <krash76@mail.ru>
2022-04-07 16:25:41 +0200

    index.js changed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {headerModal} from "./modules/headerModal.js";
 import {timer} from "./modules/timer.js";
 import {servicesModal} from "./modules/servicesModal.js";
 import {certificateModal} from "./modules/certificateModal.js";
+import {benefitsSlider} from "./modules/benefitsSlider.js";
 
 const body = document.querySelector("body");
 
@@ -11,8 +12,7 @@ body.insertAdjacentHTML("beforeend",
 			<div class="fancybox-outer">
 				<div class="fancybox-inner" style="overflow: hidden; width: auto; height: auto;">
 					<div id="callback" class="order-call-modal box-modal" style="display: block;">
-            <img class = "module-img" src="./images/documents/original/document4.jpg" style="background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" 
-             width="100%" height="100%"></img>
+            <img class = "module-img" src="./images/documents/original/document4.jpg" style="background-color: hsl(0, 0%, 90%); transition: background-color 300ms; max-width: 85vw; max-height: 90vh"></img>
           </div>
 	      </div>
       </div>
@@ -21,8 +21,8 @@ body.insertAdjacentHTML("beforeend",
   </div>`
 ); 
 
-
 headerModal();
-timer("11 July 2022");
+//timer("11 July 2022");
 servicesModal();
 certificateModal();
+benefitsSlider();
