9fb721752b29165bb8744585754322f710261300
krash76 <krash76@mail.ru>
2022-04-08 13:13:22 +0200

    benefitsSlider.js added

--- src/modules/benefitsSlider.js
+++ src/modules/benefitsSlider.js
@@ -1,49 +1,74 @@
 export const benefitsSlider = () => {
-  const leftArrow = document.querySelector(".benefits__arrow--left");
-  const rightArrow = document.querySelector(".benefits__arrow--right");
-  const sliders = document.querySelectorAll(".benefits__item");
-  let slidersArray = Array.from(sliders);
-
-  
+  const arrows = document.querySelectorAll(".benefits-arrows");
+  const slides = document.querySelectorAll(".benefits__item");
+  let currentSlide = 0;
  
-  for (let i = 3; i< sliders.length; i++) {
-      sliders[i].style.display = "none";
+  const isActive = () => {
+    slides.forEach((slide) => {
+      if (slide.classList.contains("active")){
+        slide.style.display = "block";
+        slide.style.margin = "auto"
+      } else {
+        slide.style.display = "none";
+      }
+    })
+  };
+  
+  isActive();
+   
+  const prevSlide = (elems, index) => {
+    elems[index].classList.remove("active")
+  };
+
+  const nextSlide = (elems, index) => {
+    elems[index].classList.add("active");
+  };
+  
+  const changeSlideByOne = (el) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target.closest("div").matches(".benefits__arrow--right")) {
+        prevSlide(slides, currentSlide);
+        currentSlide++;
+        if (currentSlide >= slides.length) {
+        currentSlide = 0;
+        };
+        nextSlide(slides, currentSlide);
+        isActive();
+      } else if (e.target.closest("div").matches(".benefits__arrow--left")) {
+        prevSlide(slides, currentSlide);
+        currentSlide--;
+        if (currentSlide < 0) {
+          currentSlide = slides.length - 1;
+        };
+        nextSlide(slides, currentSlide);
+        isActive(); 
+      }
+    })
+  };
+
+  for (let i = 0; i < slides.length; i++) {
+    if (window.screen.width >= 576 ) {
+      if (i < 3) {
+      slides[i].style.display = "block";
+      slides[i].classList.add("active") ;
+      } else {
+        slides[i].style.display = "none";
+      }
     }
-  
-    for (let i = 0; i < sliders.length/2; i++) {
-      sliders[i].style.display = "block";
-    }
-  
+  };
 
-  let nextIndex;
-
-  function isActive(slide) {
-    return slide.style.display === "block";
-  }
+  const changeSlideBlock = () => {
+    
+  };
   
-  function activate(slide) {
-    slide.style.display = "none";
-  }
-
-  rightArrow.addEventListener("click", () => {
-    let activeIndex = slidersArray.findIndex(isActive);
-    slidersArray[activeIndex].className = "benefits__item";
-    if (activeIndex < (slidersArray.length - 1)) {
-      nextIndex = activeIndex + 1;
+  arrows.forEach((arrow) => {
+    if (window.screen.width < 576 ) {
+      changeSlideByOne(arrow)
     } else {
-      nextIndex = 0;
+     // changeSlideBlock();
     }
-    activate(slidersArray[nextIndex]);
+    
   });
 
-  leftArrow.addEventListener("click", () => {
-    let activeIndex = slidersArray.findIndex(isActive);
-    slidersArray[activeIndex].className = "benefits__item";
-    if (activeIndex === 0) {
-      nextIndex = slidersArray.length - 1;
-    } else {
-      nextIndex = activeIndex - 1;
-    }
-    activate(slidersArray[nextIndex]);
-  })
-}
+};
