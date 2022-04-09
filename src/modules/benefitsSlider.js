0c311c0f6d00fcae558f1be1b29f722e0a98740b
krash76 <krash76@mail.ru>
2022-04-09 07:37:23 +0200

    benefitsSlider.js changed

--- src/modules/benefitsSlider.js
+++ src/modules/benefitsSlider.js
@@ -1,8 +1,11 @@
 export const benefitsSlider = () => {
   const arrows = document.querySelectorAll(".benefits-arrows");
   const slides = document.querySelectorAll(".benefits__item");
+  const slidesArray = Array.from(slides);
+  
   let currentSlide = 0;
  
+
   const isActive = () => {
     slides.forEach((slide) => {
       if (slide.classList.contains("active")){
@@ -13,8 +16,10 @@ export const benefitsSlider = () => {
       }
     })
   };
-  
-  isActive();
+ 
+
+ 
+ // isActive();
    
   const prevSlide = (elems, index) => {
     elems[index].classList.remove("active")
@@ -27,14 +32,21 @@ export const benefitsSlider = () => {
   const changeSlideByOne = (el) => {
     el.addEventListener("click", (e) => {
       e.preventDefault();
+
       if (e.target.closest("div").matches(".benefits__arrow--right")) {
-        prevSlide(slides, currentSlide);
-        currentSlide++;
+        if (window.screen.width < 576 ) {
+          prevSlide(slides, currentSlide);
+          currentSlide++;
         if (currentSlide >= slides.length) {
         currentSlide = 0;
         };
         nextSlide(slides, currentSlide);
         isActive();
+        console.log (slidesArray)
+        } else {
+
+        }
+        
       } else if (e.target.closest("div").matches(".benefits__arrow--left")) {
         prevSlide(slides, currentSlide);
         currentSlide--;
@@ -47,16 +59,29 @@ export const benefitsSlider = () => {
     })
   };
 
-  for (let i = 0; i < slides.length; i++) {
-    if (window.screen.width >= 576 ) {
-      if (i < 3) {
-      slides[i].style.display = "block";
-      slides[i].classList.add("active") ;
-      } else {
-        slides[i].style.display = "none";
-      }
-    }
+  const showSlides = () => {
+  if (window.screen.width >= 576 ) {
+    nextSlide(slides,currentSlide)
+    nextSlide(slides,(currentSlide + 1));
+    nextSlide(slides,(currentSlide + 2));
+    isActive();
+  } else {
+    prevSlide(slides, currentSlide);
+    prevSlide(slides, (currentSlide + 1));
+    prevSlide(slides, (currentSlide + 2));
+
+    nextSlide(slides,currentSlide);
+    isActive();
+  }
+    
+   
   };
+
+  showSlides();
+  window.addEventListener("resize", () => {
+    showSlides();
+  })
+ 
 
   const changeSlideBlock = () => {
     
