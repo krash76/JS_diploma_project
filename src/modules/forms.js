9a24ba1c1ac163af22cfe8d76720fbb614b3b05f
krash76 <krash76@mail.ru>
2022-04-10 21:49:52 +0200

    forms.js added

--- src/modules/forms.js
+++ src/modules/forms.js
@@ -1,23 +1,75 @@
 export const forms = () => {
 
-  const orderForms = document.querySelectorAll(".order-form.rf");
+  const horisontalForms = document.querySelectorAll(".form-horizontal");
 
-  orderForms.forEach((form) => {
+  horisontalForms.forEach((form) => {
     const nameInput = form.querySelector('input[name="fio"]');
     const phoneInput = form.querySelector('input[name="phone"]');
-    //const inputButton = form.querySelector(".order-form-button");
-   // const hiddenButtons = document.querySelectorAll(".btn-block.feedback");
+    
+    form.addEventListener("input", (e) => {
+      if (e.target === nameInput) {
+        e.target.value = e.target.value.replace(/[^а-яёa-z\s]/ig, "");
+      };
+      if (e.target === phoneInput) {
+        e.target.value = e.target.value.replace(/[^\d\+]/ig, "");
+      };
+    });
+   
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const formElements = form.querySelectorAll("input.form-control");
+     
+      const userData = {
+        name: nameInput.value,
+        tel: phoneInput.value
+      };
+      
+      const validate = (list) => {
+        let success = true;
+          list.forEach(input => {
+          inputSuccess(input);
+          if (!input.classList.contains("success")) {
+            success = false;
+          };
+        })
+        return success
+      };
 
-   form.addEventListener("input", (e) => {
-     if (e.target === nameInput) {
-       e.target.value = e.target.value.replace(/[^а-яёa-z\s]/ig, "");
-     };
+      const inputSuccess = (input) => {
+        if ((input.name === "fio" && input.value.trim().length > 1 && /^([а-яА-ЯёЁa-zA-Z\s]*)$/.test(input.value))
+          || (input.name === "phone"  && /^([\d\+]*)\d$/.test(input.value) && input.value.trim().match(/\d/g).length < 17)) {
+          input.classList.add ("success");
+        } else {
+          input.classList.add("error") ;  
+        }
+      };
+    
+      const sendData = (data) => {
+        return fetch("https://jsonplaceholder.typicode.com/posts", {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then(res => res.json())
+      };
+    
+      if (validate(formElements)) {
+        sendData(userData)
+        .then(data => {
+          formElements.forEach(input => {
+            input.value = "";
+            input.classList.remove("error"); 
+            input.classList.remove ("success"); 
+          })
+        })
+        .catch(error => {
+          console.log (error);
+        })
+      };
 
-     if (e.target === phoneInput) {
-       e.target.value = e.target.value.replace(/[^\d\+]/ig, "");
-       //e.target.value.trim().match(/\d/g).length < 17
-     };
-   }) 
+    })
   })
 }
+  
   
