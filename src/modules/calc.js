deb2544900517f3bfa2b66fd6e938d069d6dee13
krash76 <krash76@mail.ru>
2022-04-10 12:49:16 +0200

    calc.js changed

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,28 +1,32 @@
 export const calc = () => {
 
-  const calcBlock = document.getElementById("calc");
-  const calcType = document.getElementById("calc-type");
-  const calcTypeMaterial = document.getElementById("calc-type-material");
-  const calcInput = document.getElementById("calc-input");
-  const calcTotal = document.getElementById("calc-total");
-  let total;
-  
-  calcInput.addEventListener("input", (e) => {
-    e.target.value = e.target.value.replace(/[^\d]/ig, "");
-  });
+  const body = document.querySelector("body");
 
-  const toCalcTotal = () => {
-    if (calcType.value !== "--" && calcTypeMaterial.value !== "--" && calcInput.value > 0) {
-      total = calcType.value * calcTypeMaterial.value * calcInput.value;
-      calcTotal.value = Math.floor(total);
-    }
-  };
+  if (body.classList.contains("balkony" )) {
+    const calcBlock = document.getElementById("calc");
+    const calcType = document.getElementById("calc-type");
+    const calcTypeMaterial = document.getElementById("calc-type-material");
+    const calcInput = document.getElementById("calc-input");
+    const calcTotal = document.getElementById("calc-total");
+    let total;
+    
+    calcInput.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^\d]/ig, "");
+    });
 
-  calcBlock.addEventListener("input", (e) => {
-    if (e.target === calcType || e.target === calcTypeMaterial ||
-      e.target === calcInput || e.target === calcInput) {
-      toCalcTotal();  
-    }
-  });
+    const toCalcTotal = () => {
+      if (calcType.value !== "--" && calcTypeMaterial.value !== "--" && calcInput.value > 0) {
+        total = calcType.value * calcTypeMaterial.value * calcInput.value;
+        calcTotal.value = Math.floor(total);
+      }
+    };
 
+    calcBlock.addEventListener("input", (e) => {
+      if (e.target === calcType || e.target === calcTypeMaterial ||
+        e.target === calcInput || e.target === calcInput) {
+        toCalcTotal();  
+      }
+    });
+
+  } 
 }
