22e82f5da560770f1a2e9d156f427a558816314c
krash76 <krash76@mail.ru>
2022-04-12 13:56:12 +0200

    certificateModal.js changed

--- src/modules/certificateModal.js
+++ src/modules/certificateModal.js
@@ -6,8 +6,12 @@ export const certificateModal = () => {
   const certificates = document.querySelectorAll(".sertificate-document");
   const modalCloseBtn = document.querySelector(".certificate__close");
   const modalImg = document.querySelector(".modal-img");
+  
 
   certificates.forEach((cert) => {
+    if (window.screen.width < 768 ) {
+      cert.querySelector("img").setAttribute("style", "margin-bottom: 5px");
+    };
     cert.addEventListener("click", (e) => {
       e.preventDefault();
       let imgSrc = e.target.closest("a").getAttribute("href");
